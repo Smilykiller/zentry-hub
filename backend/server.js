3e98d07414abe388c8a1c2665f4712dda9489437
Smilykiller <limalkavinesh18@gmail.com>
2026-04-07 07:47:47 +0530

    Fixed Express 5 wildcard in app.options

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cors({
 }));
 
 // Explicit Preflight Handler (Stops Vercel from redirecting OPTIONS requests)
-app.options('*', cors());
+app.options(/(.*)/, cors());
 
 // Body Parsing Middleware
 app.use(express.json());
